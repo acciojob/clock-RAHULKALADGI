2e2f887b368abd7ca85ce110932e5b9c14d6c49f
Rahul R Kaladgi <127622361+RAHULKALADGI@users.noreply.github.com>
2023-04-30 21:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+var date = new Date();
+var d = date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+", ";
+const dateTime = new Date().toLocaleTimeString('en-US', {
+	hour: 'numeric', minute: 'numeric', second: 'numeric' ,hour12: true
+	})
+	document.getElementById("timer").innerHTML = d+dateTime;
